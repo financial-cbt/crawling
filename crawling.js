04e7e4dff9b0d1b2b043b18bf514876e8ceacbb9
sjun <sammanone@naver.com>
2024-02-20 15:50:51 +0900

    add Questions, options

--- crawling.js
+++ crawling.js
@@ -32,8 +32,11 @@ async function fetchPageData() {
         // console.log(urls);
 
         let questions = []
-        let answers = []
-        for (let i = 0; i < urls.length; i++) {
+        let options = []
+        for (let i = 0; i < 80; i++) {
+            if(i === 1 | i === 36 | i === 49 | i === 50) {
+                continue;
+            }
             const url = urls[i];
 
             const response = await fetch(url, {
@@ -51,11 +54,10 @@ async function fetchPageData() {
             }).get();
 
             q.pop()
-
             // console.log(q);
             questions = questions.concat(q);
 
-            const a = $('.article-body')
+            let a = $('.article-body')
                             .clone() // 원본 요소를 변경하지 않기 위해 복제합니다.
                             .find('strong') // strong 태그를 선택합니다.
                             .remove() // strong 태그를 삭제합니다.
@@ -63,21 +65,49 @@ async function fetchPageData() {
                             .text() // 텍스트를 추출합니다.
                             .trim(); // 앞뒤 공백을 제거합니다.
 
-            console.log(a)
-        }
-        
-        console.log(questions.length);
-        
-        // // 객체 생성
-        // const obj = title.map((title, i) => ({
-        //     id: i + 1,
-        //     title: title,
-        //     newspaper: newspaper[i],
-        //     context: context[i],
-        //     img: img[i],
-        //     detailText: detailText[i]
-        // }));
+            a = a.split('①');
+            for (let i = 0; i < a.length; i++) {
+                a[i] = '①' + a[i].replace(/\s/g, '');
 
+                const regex = /①|②|③|④/g;
+
+                // 문자열을 분리하여 배열로 변환
+                const result = a[i].split(regex);
+                result.shift()
+
+                a[i] = result.map((item, index) => {
+                    if (index === 0) {
+                        return item = '①' + item
+                    }
+                    else if (index === 1) {
+                        return item = '②' + item
+                    }
+                    else if (index === 2) {
+                        return item = '③' + item
+                    }
+                    else {
+                        return item = '④' + item
+                    }
+                });
+            }
+            a.shift()
+            options = options.concat(a)
+        }
+        console.log(questions.length);
+        // console.log(questions)
+        console.log(options.length);
+        // console.log(options)
+        
+        // 객체 생성
+        const obj = options.map((title, i) => ({
+            num: i + 1,
+            questions: questions[i],
+            options: options
+        }));
+
+        // console.log(obj)
+
+        
         // // json 파일 저장
         // fs.writeFile( 'crolling2.json', JSON.stringify(obj), function(err) {
         //     console.log( 'json파일 생성완료' );
